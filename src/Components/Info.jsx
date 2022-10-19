7dad9a22aa9245082d3a68318d50f7ff1ae869d3
Aaron <Harvey.AaronJ@gmail.com>
2022-10-19 00:01:10 -0400

    .

--- src/Components/Info.jsx
+++ src/Components/Info.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
 export default function Info () {
-	return <img src='./src/Images/Photo.jpg' />;
+	return (
+		<div className='info'>
+			<img src='./src/Images/Photo.jpg' height='250px' />
+			<h1>Aaron Harvey</h1>
+			<h5>Fullstack Developer</h5>
+		</div>
+	);
 }
