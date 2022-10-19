10749a6dd86e6faec605e9a590037b76f276874d
Aaron <Harvey.AaronJ@gmail.com>
2022-10-18 23:14:02 -0400

    Imports About, Footer, Info, and Interests to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import React from 'react';
+import About from './Components/About';
+import Footer from './Components/Footer';
+import Info from './Components/Info';
+import Interests from './Components/Interests';
+
 function App () {
-	return <h1>Test</h1>;
+	return (
+		<div>
+			<Info />
+			<About />
+			<Interests />
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
