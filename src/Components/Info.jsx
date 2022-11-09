693ca8af85f7de5b951e2b135550985231ed33c9
Aaron <Harvey.AaronJ@gmail.com>
2022-11-09 10:38:23 -0500

    Adds layout change of Info

--- src/Components/Info.jsx
+++ src/Components/Info.jsx
@@ -3,9 +3,14 @@ import React from 'react';
 export default function Info () {
 	return (
 		<div className='info'>
-			<img id='photo' src='./src/Images/Photo.jpg' />
-			<h1>Aaron Harvey</h1>
-			<h5>Fullstack Developer</h5>
+			<div id='info-photo-container'>
+				<img id='photo' src='./src/Images/Photo.jpg' />
+			</div>
+			<div id='info-text'>
+				<h1>Aaron Harvey</h1>
+				<h5>Fullstack Developer</h5>
+				<p>website goes here </p>
+			</div>
 		</div>
 	);
 }
