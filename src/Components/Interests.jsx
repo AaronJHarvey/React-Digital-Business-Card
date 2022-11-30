980dc45fda9af35baaa0c0f37c66c0ade69d96de
Aaron <Harvey.AaronJ@gmail.com>
2022-11-30 09:49:57 -0500

    Adds content to Interests

--- src/Components/Interests.jsx
+++ src/Components/Interests.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
 export default function Interests () {
-	return <h1> Interests goes here</h1>;
+	return (
+		<div id=''>
+			<h1>Interests</h1>
+			<p>Comicbook nerd. Bookworm. Movie Buff. Traveler. Permanently lost in fantasy and sci-fi worlds.</p>
+		</div>
+	);
 }
