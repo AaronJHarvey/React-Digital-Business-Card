ea98bf4560c046b1d6617be2519b1f8622fb92e6
Aaron <Harvey.AaronJ@gmail.com>
2022-11-30 09:57:43 -0500

    Changes some wording in About/Interests

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -7,7 +7,8 @@ export default function About () {
 			<p>
 				I am a fullstack developer with a particular interest in the frontend. I enjoy making things simple and
 				easy to navigate and understand for the user while also ensuring they are well built, maintained, and
-				working properly so as to create a more seemless experience.
+				working properly so as to create a more seemless experience. Put another way - I'm constantly trying to
+				make computers do their computer things.
 			</p>
 		</div>
 	);
--- src/Components/Interests.jsx
+++ src/Components/Interests.jsx
@@ -4,7 +4,10 @@ export default function Interests () {
 	return (
 		<div id=''>
 			<h1>Interests</h1>
-			<p>Comicbook nerd. Bookworm. Movie Buff. Traveler. Permanently lost in fantasy and sci-fi worlds.</p>
+			<p>
+				Comicbook nerd. Bookworm. Movie Buff. Traveler. Social/Economic Change Advocate. Eternal student.
+				Permanently "lost" in Fantasy and Sci-Fi worlds.
+			</p>
 		</div>
 	);
 }
