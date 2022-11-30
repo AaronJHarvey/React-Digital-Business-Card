70090ceddec1a28611cdd1ffdb791147c95708fa
Aaron <Harvey.AaronJ@gmail.com>
2022-11-30 11:34:55 -0500

    Builds Buttons/Content for Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import { FaGithubSquare } from 'react-icons/fa';
+import { IoLogoVenmo } from 'react-icons/io5';
+import { handleClick } from '../Utils';
 
 export default function Footer () {
-	return <h1 id='footer-text'> Footer goes here</h1>;
+	// const handleClick = (destination) => {
+	// 	window.open(destination, '_blank');
+	// };
+
+	return (
+		<div className='footer-container'>
+			<button onClick={() => handleClick('https://github.com/AaronJHarvey')} id='github-button'>
+				<FaGithubSquare />
+			</button>
+			<button onClick={() => handleClick('https://account.venmo.com/u/AaronJHarvey')} id='venmo-button'>
+				<IoLogoVenmo />
+			</button>
+		</div>
+	);
 }
