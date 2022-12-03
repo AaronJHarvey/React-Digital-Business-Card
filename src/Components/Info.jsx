6e5cc755e0710f733992c3cf09c195f7c539c530
Aaron <Harvey.AaronJ@gmail.com>
2022-12-03 09:44:49 -0500

    .

--- src/Components/Info.jsx
+++ src/Components/Info.jsx
@@ -7,7 +7,7 @@ export default function Info () {
 	return (
 		<div className='info'>
 			<div id='info-photo-container'>
-				<img id='photo' src='./src/Images/Photo.jpg' />
+				<img id='photo' src='/src/Images/Photo.jpg' />
 			</div>
 			<div id='info-text'>
 				<h1>Aaron Harvey</h1>
